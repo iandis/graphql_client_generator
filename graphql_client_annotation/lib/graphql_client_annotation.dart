7dea1a2a65f049b9e5508f8faef8233475e75b41
Iandi Santulus <iandisantulusn@gmail.com>
2023-01-31 23:08:05 +0700

    feat(annotation): add query result mapper

--- graphql_client_annotation/lib/graphql_client_annotation.dart
+++ graphql_client_annotation/lib/graphql_client_annotation.dart
@@ -1,4 +1,5 @@
 import 'package:graphql/client.dart';
+import 'package:meta/meta.dart';
 import 'package:meta/meta_meta.dart';
 
 enum GQLType {
@@ -6,45 +7,56 @@ enum GQLType {
   query,
 }
 
+typedef QueryResultMapper = Object Function<T>(QueryResult<T> result);
+
 const GQLClient gqlClient = GQLClient();
 
 @Target({TargetKind.classType})
 class GQLClient {
-  const GQLClient();
+  const GQLClient({this.mapper});
+
+  final QueryResultMapper? mapper;
 }
 
+@optionalTypeArgs
 @Target({TargetKind.method})
-class GQL {
+class GQL<T> {
   const GQL(
     this.type,
     this.body, {
     required this.parser,
+    this.mapper,
     this.name,
     this.fetchPolicy,
   }) : assert(body.length > 0, 'Body must not be empty');
 
   final GQLType type;
   final String body;
+  final T Function(Map<String, dynamic> json) parser;
+  final QueryResultMapper? mapper;
   final String? name;
   final FetchPolicy? fetchPolicy;
-  final Object? Function(Map<String, dynamic> json) parser;
 }
 
+@optionalTypeArgs
 @Target({TargetKind.method})
-class Mutation extends GQL {
+class Mutation<T> extends GQL<T> {
   const Mutation(
     String body, {
     required super.parser,
+    super.mapper,
     super.name,
     super.fetchPolicy,
   }) : super(GQLType.mutation, body);
 }
 
+@optionalTypeArgs
 @Target({TargetKind.method})
-class Query extends GQL {
+class Query<T> extends GQL<T> {
   const Query(
     String body, {
     required super.parser,
+    super.mapper,
     super.name,
     super.fetchPolicy,
   }) : super(GQLType.query, body);
